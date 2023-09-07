a85c3338370f33c89e60d530bc54fbb680c3c2fe
LotteDos <Lottedos@live.be>
2023-09-07 22:09:31 +0200

    Give mobs custom hitboxes
    Hit box is decided based on randomly chosen animation

--- Demo's/celeryman/actors/mob.gd
+++ Demo's/celeryman/actors/mob.gd
@@ -1,21 +1,25 @@
 extends RigidBody2D
 
 
-# Called when the node enters the scene tree for the first time.
 
+# --- Standard ---
 func _ready():
 	var mob_types = $AnimatedSprite2D.sprite_frames.get_animation_names()
-	$AnimatedSprite2D.play(mob_types[randi() % mob_types.size()])
-	#collision_mask = 1 -> test, now i gotta bind it to button
+	var mob_type = mob_types[randi() % mob_types.size()]
+	var model_table = { # A dictionary / table # (key -> value) # value = table.get(key)
+		"mob1" : $ShapeMob1, # key : value
+		"mob2" : $ShapeMob2,
+		"mob3" : $ShapeMob3
+	}
+	model_table.get(mob_type).disabled = false
+	$AnimatedSprite2D.play(mob_type)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 
 func _process(delta):
 	pass
 
 
+
+# --- Listeners ---
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
-
-#if var physics = true:
-
